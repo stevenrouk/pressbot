65d125ba08bf6a61da1159f5229892fe697f7a6c
Steven Rouk <stevenrouk@gmail.com>
2016-09-21 17:14:26 -0600

    Display index page that says 'Hello world'

--- pressbot/urls.py
+++ pressbot/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import articles.views
+
 urlpatterns = [
+    url(r'^$', articles.views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
