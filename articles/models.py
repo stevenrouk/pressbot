ce6ca7882557987d8a254b57ab450433eedde015
Steven Rouk <stevenrouk@gmail.com>
2016-10-09 11:39:47 -0600

    Update models to include html text.

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,6 @@
+import bs4 as BeautifulSoup
 import datetime
+import requests
 
 from django.db import models
 from django.urls import reverse
@@ -9,9 +11,26 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class PressRelease(models.Model):
     url = models.URLField()
+    html = models.TextField(default='')
+    p_tag_text = models.TextField(default='')
 
     def __str__(self):
         return self.url
 
+    def save(self):
+        self.html = self.get_html(self.url)
+        self.p_tag_text = self.get_p_tag_text(self.html)
+        super(PressRelease, self).save()
+
     def get_absolute_url(self):
         return reverse('press-release-detail', kwargs={'pk': self.pk})
+
+    def get_html(self, url):
+        return requests.get(url).text
+
+    def get_p_tag_text(self, html):
+        soup = BeautifulSoup.BeautifulSoup(html, 'html.parser')
+        p_tags = soup.findAll('p')
+        p_text = '\n'.join([tag.text for tag in p_tags])
+        clean_p_text = ' '.join(p_text.split())
+        return clean_p_text
