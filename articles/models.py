27ccf01a43bc4ed4ebd40752712173afe2eb5cea
Steven Rouk <stevenrouk@gmail.com>
2016-10-25 13:15:32 -0600

    Add sentiment to model and templates

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 import bs4 as BeautifulSoup
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import requests
 
 from django.db import models
@@ -13,6 +14,7 @@ class PressRelease(models.Model):
     p_tag_text = models.TextField(default='')
     p_tag_text_char_count = models.IntegerField(default=0)
     p_tag_text_word_count = models.IntegerField(default=0)
+    polarity_score = models.TextField(default='')
 
     def __str__(self):
         return self.url
@@ -26,6 +28,8 @@ class PressRelease(models.Model):
             self.p_tag_text_char_count = len(self.p_tag_text)
         if self.p_tag_text_word_count == 0:
             self.p_tag_text_word_count = len(self.p_tag_text.split())
+        if not self.polarity_score:
+            self.polarity_score = self.get_sentiment_polarity_scores(self.p_tag_text)
         super(PressRelease, self).save()
 
     def get_absolute_url(self):
@@ -40,3 +44,7 @@ class PressRelease(models.Model):
         p_text = '\n'.join([tag.text for tag in p_tags])
         clean_p_text = ' '.join(p_text.split())
         return clean_p_text
+
+    def get_sentiment_polarity_scores(self, text):
+        sid = SentimentIntensityAnalyzer()
+        return sid.polarity_scores(text)
