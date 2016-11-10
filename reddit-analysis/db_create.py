035c5ffd45b8e5140518a9427997009a93dd314a
Steven Rouk <stevenrouk@gmail.com>
2016-11-10 14:43:41 -0700

    Fix bugs in the process of adding scraped data to db.

--- reddit-analysis/db_create.py
+++ reddit-analysis/db_create.py
@@ -13,18 +13,15 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     reddit_id = Column(String(250), nullable=False)
     reddit_fullname = Column(String(250))
-    author_name = Column(String(250))
-    author_id = Column(String(250))
-    published_date = Column(DateTime)
+    created_date = Column(DateTime)
     title = Column(String(250))
     url = Column(String(250))
-    text = Column(String(250))
     num_comments = Column(Integer)
     score = Column(Integer)
     upvotes = Column(Integer)
     downvotes = Column(Integer)
     comments = relationship('Comment', backref='post')
-    author = relationship('Author', backref=backref('post', uselist=False))
+    author = relationship('Author', backref='post')
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -32,9 +29,10 @@ class Comment(Base):
     id = Column(Integer, primary_key=True)
     reddit_id = Column(String(250), nullable=False)
     reddit_fullname = Column(String(250))
-    published_date = Column(DateTime)
+    created_date = Column(DateTime)
     text = Column(String(250))
     post_id = Column(Integer, ForeignKey('post.id'))
+    author = relationship('Author', backref='comment')
 
 class Author(Base):
     __tablename__ = 'author'
@@ -45,6 +43,7 @@ class Author(Base):
     name = Column(String(250))
     created_date = Column(DateTime)
     post_id = Column(Integer, ForeignKey('post.id'))
+    comment_id = Column(Integer, ForeignKey('comment.id'))
 
 # Create database
 engine = create_engine(SQLITE_DATABASE_URI)
--- reddit-analysis/scrape_reddit.py
+++ reddit-analysis/scrape_reddit.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import praw
@@ -6,7 +7,9 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 from config import REDDIT_USER_AGENT, SQLITE_DATABASE_URI
-from db_create import Base, Comment, Post
+from db_create import Author, Base, Comment, Post
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
 ##########################
 ##########################
@@ -32,6 +35,8 @@ def get_db_session():
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
+
+    return session
 
 def get_posts(subreddit='vegan', post_type='hot', limit=1000):
     # Set up Reddit objects
@@ -62,12 +67,12 @@ def save_posts(posts):
             # Add author first
             new_author = get_object(session, Author, reddit_id=post.author.id)
             if not new_author:
-                author = new_post.author
+                author = post.author
                 new_author = Author(
                     reddit_id=author.id,
                     reddit_fullname=author.fullname,
                     name=author.name,
-                    created_date=author.created,
+                    created_date=datetime.datetime.fromtimestamp(author.created),
                 )
                 session.add(new_author)
                 session.commit()
@@ -76,11 +81,10 @@ def save_posts(posts):
             new_post = Post(
                 reddit_id=post.id,
                 reddit_fullname=post.fullname,
-                author=new_author,
-                created_date=post.created,
+                author=[new_author],
+                created_date=datetime.datetime.fromtimestamp(post.created),
                 title=post.title,
                 url=post.url,
-                text=post.text,
                 num_comments=post.num_comments,
                 score=post.score,
                 upvotes=post.ups,
@@ -90,6 +94,7 @@ def save_posts(posts):
             session.commit()
 
         # Add comments
+        post.replace_more_comments(limit=None, threshold=0)
         for comment in post.comments:
             new_comment = get_object(session, Comment, reddit_id=comment.id)
             if not new_comment:
@@ -101,7 +106,7 @@ def save_posts(posts):
                         reddit_id=author.id,
                         reddit_fullname=author.fullname,
                         name=author.name,
-                        created_date=author.created,
+                        created_date=datetime.datetime.fromtimestamp(author.created),
                     )
                     session.add(new_author)
                     session.commit()
@@ -109,27 +114,99 @@ def save_posts(posts):
                 # Add comment
                 new_comment = Comment(
                     reddit_id=comment.id,
-                    created_date=comment.created,
+                    created_date=datetime.datetime.fromtimestamp(comment.created),
                     text=comment.body,
-                    author=new_author,
+                    author=[new_author],
                     post=new_post,
                 )
                 session.add(new_comment)
                 session.commit()
 
 
+def mvp_scrape(subreddit='vegan', post_type='hot', limit=10):
+    """
+    Scrape posts and comments, dump them to text.
+    Just to validate some ideas.
+    """
+    print('Scraping for {}'.format(subreddit))
+    file_name = os.path.join(BASEDIR, 'data', 'mvp-{}-subreddit.txt'.format(subreddit))
+    post_count = 0
+    comment_count = 0
+    total_comment_count = 0
+    more_comments_count = 0
+    # Set up Reddit objects
+    user_agent = REDDIT_USER_AGENT
+    r = praw.Reddit(user_agent=user_agent)
+    subreddit_object = r.get_subreddit(subreddit)
 
+    # Get posts from the vegan subreddit
+    if post_type == 'hot':
+        posts = subreddit_object.get_hot(limit=limit)
+    elif post_type == 'top':
+        posts = subreddit_object.get_top(limit=limit)
+    elif post_type == 'new':
+        posts = subreddit_object.get_new(limit=limit)
+    else:
+        posts = None
+
+    for post in posts:
+        comment_count = 0
+        try:
+            with open(file_name, 'a') as f:
+                f.write(post.title)
+                f.write('\n')
+                post_count += 1
+                print('wrote post #{}'.format(post_count))
+            for comment in post.comments:
+                if type(comment) == praw.objects.MoreComments:
+                    more_comments_count += 1
+                else:
+                    with open(file_name, 'a') as f:
+                        f.write(comment.body)
+                        f.write('\n')
+                        comment_count += 1
+                        total_comment_count += 1
+                        print('wrote comment #{0} (#{1} overall)'.format(comment_count, total_comment_count))
+        except Exception as e:
+            print(e)
+
+    print("""
+Done executing for {3}.\n\tTotal Posts: {0}\n\tTotal Comments: {1}\n\t Skipped 'More Comments': {2}\n
+""".format(post_count, total_comment_count, more_comments_count, subreddit))
+    print('\n\n*****************************\n\n')
+
+def mvp():
+    list_of_subreds = [
+        'animalrights',
+        'animalwelfare',
+        'veg',
+        'vegetarian',
+        'vegetarianism',
+        'dietaryvegan',
+        'veganrecipes',
+        'vegproblems',
+    ]
+    for subred in list_of_subreds:
+        try:
+            mvp_scrape(subreddit=subred, limit=100)
+        except Exception as e:
+            print('Failed for {0}: {1}'.format(subred, e))
 
 if __name__ == '__main__':
     # TODO: Check script args.
     #     - If we tell it to run continuously, do that, but the default is a small test.
-    if sys.argv[1] == 'continuous':
+    
+    if len(sys.argv) == 1:
+        posts = get_posts(subreddit='vegan', post_type='hot', limit=10)
+        save_posts(posts)
+    elif sys.argv[1] == 'continuous':
         while True:
             for post_type in ('hot', 'new', 'top'):
                 posts = get_posts(post_type)
                 save_posts(posts)
         posts = get_posts(subreddit='vegan', post_type='hot', limit=10)
         save_posts(posts)
+    elif sys.argv[1] == 'mvp':
+        mvp_scrape()
     else:
-        posts = get_posts(subreddit='vegan', post_type='hot', limit=10)
-        save_posts(posts)
+        pass
